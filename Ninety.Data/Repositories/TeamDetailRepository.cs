4c8ddf2138609119eb5eb08ed7ab9f764396f8c5
HoangTien0105 <hoangtien01052003@gmail.com>
2024-10-07 20:06:52 +0700

    add create team api

---
+++ Ninety.Data/Repositories/TeamDetailRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ninety.Data.Repositories.Interfaces;
+using Ninety.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninety.Data.Repositories
+{
+    public class TeamDetailRepository : ITeamDetailsRepository
+    {
+        private readonly NinetyContext _context;
+
+        public TeamDetailRepository(NinetyContext context)
+        {
+            _context = context;
+        }
+        public async Task<TeamDetail> Create(TeamDetail team)
+        {
+            // Sử dụng Transaction để đảm bảo tính toàn vẹn dữ liệu
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    await _context.TeamDetails.AddAsync(team);
+                    await _context.SaveChangesAsync();
+
+                    // Commit transaction sau khi các thao tác đã thành công
+                    await transaction.CommitAsync();
+                }
+                catch (Exception)
+                {
+                    // Rollback transaction nếu có lỗi
+                    await transaction.RollbackAsync();
+                    throw;
+                }
+            }
+
+            return team;
+        }
+
+        public async Task<List<TeamDetail>> GetAll()
+        {
+            return await _context.TeamDetails.ToListAsync();
+        }
+
+        public async Task<TeamDetail> GetById(int id)
+        {
+            return await _context.TeamDetails.FirstOrDefaultAsync(e => e.Id == id);
+        }
+    }
+}
--- Ninety.Models/DTOs/Request/CreateTeamRequestDTO.cs
+++ Ninety.Models/DTOs/Request/CreateTeamRequestDTO.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Ninety.Models.DTOs.Request
 {
-    internal class CreateTeamRequestDTO
+    public class CreateTeamRequestDTO
     {
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public int TournamentId { get; set; }
+        public int UserId { get; set; }
     }
 }
