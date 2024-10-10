dc7af142a3c0ee994a8461a47e05bf1daae45dfa
TyKay264 <khaipt4working@gmail.com>
2024-10-11 00:33:24 +0700

    feat/fix-base-response

--- Ninety.Models/DTOs/Response/BaseResponse.cs
+++ Ninety.Models/DTOs/Response/BaseResponse.cs
@@ -13,11 +13,11 @@ namespace Ninety.Models.DTOs.Response
         public bool IsSuccess { get; set; }
         public object? Data { get; set; } = null!;
         public IDictionary<string, string[]>? Errors { get; set; } = null!;
-        public int CurrentPage { get; private set; }
-        public int TotalPages { get; private set; }
-        public int PageSize { get; private set; }
-        public int TotalCount { get; private set; }
-        public bool HasPrevious => CurrentPage > 1;
-        public bool HasNext => CurrentPage < TotalPages;
+        public int? CurrentPage { get; set; }
+        public int? TotalPages { get; set; }
+        public int? PageSize { get; set; }
+        public int? TotalCount { get; set; }
+        public bool? HasPrevious => CurrentPage > 1;
+        public bool? HasNext => CurrentPage < TotalPages;
     }
 }
